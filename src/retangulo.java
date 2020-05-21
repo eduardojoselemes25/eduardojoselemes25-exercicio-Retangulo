1acb84aae9e4820bf99a7fa2c6a5b39f2e46e861
eduardojoselemes25 <50559973+eduardojoselemes25@users.noreply.github.com>
2020-05-20 22:35:38 -0300

    Programa finalizado

--- src/retangulo.java
+++ src/retangulo.java
@@ -20,6 +20,34 @@ public class retangulo {
 		while (altura <= 0) {
 			System.out.print("O valor deve ser positivo. Tente novamente: ");
 			altura = sc.nextDouble();
+		}
+		System.out.println();
+		int opcao = 0;
+		while (opcao != 3) {
+			System.out.println("MENU: ");
+			System.out.println("1 - Mostrar area ");
+			System.out.println("2 - Mostrar perimetro ");
+			System.out.println("3 - Sair ");
+			System.out.print("Digite uma opcao: ");
+			opcao = sc.nextInt();
+			if (opcao <= 0 || opcao > 3) {
+				System.out.println();
+				System.out.println("OPÇÃO INVALIDA");
+				System.out.println();
+			} else if (opcao == 1) {
+				double area = largura * altura;
+				System.out.println();
+				System.out.printf("AREA = %.1f%n", area);
+				System.out.println();
+			} else if (opcao == 2) {
+				double perimetro = 2 * (largura + altura);
+				System.out.println();
+				System.out.printf("PERIMETRO = %.1f%n", perimetro);
+				System.out.println();
+			}
+		}
+		System.out.println();
+		System.out.printf("FIM DO PROGRAMA!");
+		sc.close();
 	}
-}
 }
